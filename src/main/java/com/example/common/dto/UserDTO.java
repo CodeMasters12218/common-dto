4f3a319e7e2e2f1f6fb9e429d15065025fcfea75
CodeMasters12218 <eowinmur@gmail.com>
2025-11-24 16:10:30 +0100

    Commit inicial

---
+++ src/main/java/com/example/common/dto/UserDTO.java
@@ -0,0 +1,35 @@
+﻿package com.example.common.dto;
+
+import java.util.List;
+
+public class UserDTO {
+    private Integer id;
+    private String name;
+    private List<ProductDTO> products;
+
+    public UserDTO() {}
+    public UserDTO(Integer id, String name, List<ProductDTO> products) {
+        this.id = id;
+        this.name = name;
+        this.products = products;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<ProductDTO> getProducts() {
+        return products;
+    }
+    public void setProducts(List<ProductDTO> products) {
+        this.products = products;
+    } 
+}
+
